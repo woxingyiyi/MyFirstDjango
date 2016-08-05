083579d9d27062d3cd227face0b7397ffb9a72d9
woxingyiyi <yctctangcheng@outlook.com>
2016-08-05 17:38:30 +0800

    set home page

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^blog/', include('blog.urls', namespace='blog', app_name='blog')),
     url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    url(r'', include('blog.urls')),
 ]
