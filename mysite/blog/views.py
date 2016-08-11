5ed230e92fdc816cfe9329af984c7b5f4832e7c2
woxingyiyi <yctctangcheng@outlook.com>
2016-08-11 12:28:14 +0800

    remove email share, use js script share

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -7,7 +7,7 @@ from django.views.generic import ListView
 from taggit.models import Tag
 
 from .models import Post, Comment
-from .forms import EmailPostForm, CommentForm
+from .forms import CommentForm
 # Create your views here.
 
 def post_list(request, tag_slug=None):
@@ -54,22 +54,6 @@ class PostListView(ListView):
     paginate_by = 3
     template_name = 'blog/post/list.html' 
 
-def post_share(request, post_id):
-    post = get_object_or_404(Post, id=post_id, status='published')
-    sent = False
-
-    if request.method == 'POST':
-        form = EmailPostForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            post_url = request.build_absolute_uri(post.get_absolute_url())
-            subject = '{} {{}} recommends you reading "{}"'.format(cd['name'], cd['email'], post.title)
-            message = 'Read "{}" at {}\n\n{}\'s comments:{}'.format(post.title, post_url, cd['name'], cd['comments'])
-            send_mail(subject, message, 'admin@myblog.com', [cd['to']])
-            sent = True 
-    else:
-        form = EmailPostForm()
-    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
     
 
     
